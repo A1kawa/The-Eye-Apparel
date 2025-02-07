235560c0896b30e5522e5323e74792170acda310
A1kawa <worldg555@gmail.com>
2025-02-07 17:18:09 -0300

    orders

---
+++ prod.js
@@ -0,0 +1,66 @@
+let imgs = (document.getElementsByClassName('NavImg').length) -1
+let quantity = 1
+let color = 0
+let size = 0
+sessionStorage.setItem("prods", 0)
+
+window.onload = function(){
+    document.getElementsByClassName('NavImg')[0].style.opacity = 1
+}
+
+function setImg(param) {
+    const len = imgs
+    while (imgs >= 0) {
+        document.getElementsByClassName('NavImg')[imgs].style.opacity = .5
+        imgs--
+    }
+    document.documentElement.style.setProperty("--img-row", ((param - 1) * 100))
+    document.getElementsByClassName('NavImg')[(param - 1)].style.opacity = 1
+    imgs = len
+}
+function setDesc(param){
+    if (param <= 3) {
+        let ind = 3
+        while (ind >= 0) {
+            document.getElementsByClassName('but')[ind].style.backgroundColor = ''
+            document.getElementsByClassName('but')[ind].style.color = ''
+            ind--
+        }
+        document.getElementsByClassName('but')[param].style.backgroundColor = '#D9D9D9'
+        document.getElementsByClassName('but')[param].style.color = '#009AC2'
+        size = (document.getElementsByClassName('but')[param].innerText)
+    } else {
+        let ind = (document.getElementsByClassName('col').length) - 1
+        while (ind >= 0) {
+            document.getElementsByClassName('col')[ind].style.backgroundColor = ''
+            document.getElementsByClassName('col')[ind].style.color = ''
+            ind--
+        }
+        document.getElementsByClassName('col')[param - 4].style.backgroundColor = '#D9D9D9'
+        document.getElementsByClassName('col')[param - 4].style.color = '#009AC2'
+        color = (document.getElementsByClassName('col')[param - 4].innerText)
+    }
+}
+function setQnt(param){
+    if (param == 0 && quantity > 1) {
+        quantity--
+    } else if (param == 1) {
+        quantity++
+    }
+    document.getElementById('qnt').innerText = quantity
+}
+
+function add() {
+    if (color == 0 || size == 0) {
+        console.log(color, size)
+        localStorage.clear()
+        return;
+    } else {
+        const prod = {name: (document.getElementById("name").innerText), color: color, size: size, qnt: quantity}
+        sessionStorage.setItem("prods", (Number.parseInt(sessionStorage.getItem("prods")) + 1))
+        sessionStorage.setItem(`product0${sessionStorage.getItem("prods")}`, JSON.stringify(prod));
+        console.log(prod)
+        console.log(sessionStorage.getItem("prods"))
+        console.log(JSON.parse(sessionStorage.getItem("product01")))
+    }
+}
